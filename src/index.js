9d16be6fceb36cda017f50aa602d676744088012
ameeking <aaronmeeking@gmail.com>
2020-11-04 14:03:05 +1100

    component index

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as Button } from "./components/Button.vue";
+export { default as Card } from "./components/Card.vue";
+export { default as Chip } from "./components/Chip.vue";
+export { default as Grid } from "./components/Grid.vue";
+export { default as GridCol } from "./components/GridCol.vue";

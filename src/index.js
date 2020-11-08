3f6e4f8ed5661e3ed5df31309daf068ef2e25285
ameeking <aaronmeeking@gmail.com>
2020-11-08 21:40:18 +1100

    Adding Tile to index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as Card } from "./components/Card.vue";
 export { default as Chip } from "./components/Chip.vue";
 export { default as Grid } from "./components/Grid.vue";
 export { default as GridCol } from "./components/GridCol.vue";
+export { default as Tile } from "./components/Tile.vue";
